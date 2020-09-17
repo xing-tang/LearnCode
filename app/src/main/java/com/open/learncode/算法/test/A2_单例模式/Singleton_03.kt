e998b2b02f93b6e3fad23f5fa201de5dac332881
xing.tang <cnxing.tang@gmail.com>
2020-09-17 11:56:26 +0800

    更新

--- app/src/main/java/com/open/learncode/算法/test/A2_单例模式/Singleton_03.kt
+++ app/src/main/java/com/open/learncode/算法/test/A2_单例模式/Singleton_03.kt
@@ -1,9 +1,9 @@
 package com.open.learncode.算法.test.A2_单例模式
 
-class Singltton_03 {
+class Singleton_03 {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {}
-        val instance: Singltton_03 by lazy { Singltton_03() }
+        val instance: Singleton_03 by lazy { Singleton_03() }
     }
 }
