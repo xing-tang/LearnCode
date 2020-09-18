bd59396079e8eb06c3559bd2fa3e961426f80a1b
xing.tang <cnxing.tang@gmail.com>
2020-09-18 15:44:38 +0800

    更新

--- app/src/main/java/com/open/learncode/算法/test/A5_跟数组相关的算法/JZ05_2_合并两个排序数组.java
+++ app/src/main/java/com/open/learncode/算法/test/A5_跟数组相关的算法/JZ05_2_合并两个排序数组.java
@@ -1,5 +1,7 @@
 package com.open.learncode.算法.test.A5_跟数组相关的算法;
 
+import com.open.learncode.算法.base.PrintUtils;
+
 /**
  * 题目：
  * 有两个排序的数组A1和A2，内存在A1的末尾有足够多的空余空间容纳A2。
@@ -16,13 +18,11 @@ public class JZ05_2_合并两个排序数组 {
 
     public static void main(String[] args) {
         int[] A1 = new int[7];
-        A1[0] = 1;
-        A1[1] = 2;
-        A1[2] = 3;
-        A1[3] = 4;
+        for (int i = 0; i < 4; i++)
+            A1[i] = i + 1;
         int A1Size = 4;
         int[] A2 = {2, 4, 6};
-        method(A1, A1Size, A2);
+        PrintUtils.getInstance().printIntArray(method(A1, A1Size, A2));
     }
 
     /**
@@ -31,17 +31,17 @@ public class JZ05_2_合并两个排序数组 {
      * @param A1     输入的数组A1
      * @param A1Size A1数组的真实长度
      * @param A2     输入的数组A2
-     * @return 返回新的数组
+     * @return 返回合并后的数组A1
      */
     public static int[] method(int[] A1, int A1Size, int[] A2) {
         if (A1 == null || A1.length <= 0) return A2;
         if (A2 == null || A2.length <= 0) return A1;
         if (A1.length < (A1Size + A2.length)) return null;
-        int a = A1Size - 1;
-        int b = A2.length - 1;
-        int len = a + b + 1;
-        while (a >= 0 && b >= 0) {
-            A1[len--] = A1[a] > A2[b] ? A1[a--] : A2[b--];
+        int a1Index = A1Size - 1;
+        int a2Index = A2.length - 1;
+        int len = a1Index + a2Index + 1;
+        while (a1Index >= 0 && a2Index >= 0) {
+            A1[len--] = A1[a1Index] > A2[a2Index] ? A1[a1Index--] : A2[a2Index--];
         }
         return A1;
     }
--- app/src/main/java/com/open/learncode/算法/test/A8_链表相关算法/JZ24_1_反转链表.java
+++ app/src/main/java/com/open/learncode/算法/test/A8_链表相关算法/JZ24_1_反转链表.java
@@ -14,7 +14,8 @@ import com.open.learncode.算法.base.PrintUtils;
  * 在改变节点h的next指向pre的同时，需要保存节点tempNext，防止链表断裂
  * <p>
  * 复杂度分析：
- * 时间复杂度：O(n)，空间复杂度：O(1)
+ * 方法一：时间复杂度：O(n)，空间复杂度：O(1)
+ * 方法二：时间复杂度：O(n)，空间复杂度：O(n)
  */
 public class JZ24_1_反转链表 {
 
@@ -26,7 +27,8 @@ public class JZ24_1_反转链表 {
         ListNode<Integer> node1 = new ListNode<Integer>(1, node2);
         // 测试用例
         PrintUtils.getInstance().printListNode(node1);
-        PrintUtils.getInstance().printListNode(method_1(node1), "三指针反转后的链表为");
+        //PrintUtils.getInstance().printListNode(method_1(node1), "三指针反转后的链表为");
+        PrintUtils.getInstance().printListNode(method_2(node1), "递归反转后的链表为");
     }
 
     /**
@@ -48,5 +50,22 @@ public class JZ24_1_反转链表 {
             curr = next;
         }
         return pre;
+    }
+
+    public static ListNode<Integer> method_2(ListNode<Integer> head) {
+        // 鲁棒性
+        if (head == null || head.next == null) return head;
+
+        // 这里的cur就是最后一个节点
+        ListNode<Integer> cur = method_2(head.next);
+        // 这里请配合动画演示理解
+        // 如果链表是 1->2->3->4->5，那么此时的cur就是5
+        // 而head是4，head的下一个是5，下下一个是空
+        // 所以head.next.next 就是5->4
+        head.next.next = head;
+        // 防止链表循环，需要将head.next设置为空
+        head.next = null;
+        // 每层递归函数都返回cur，也就是最后一个节点
+        return cur;
     }
 }
