bccdde74f059c533d806ab73b348a4e1bb7158bc
mengjia.tang <mengjia.tang@appshahe.com>
2022-05-11 19:07:48 +0800

    mod: 扑克牌的顺子正确解法.

--- app/app-learncode/src/main/java/com/open/learncode/算法/java/剑指offer/A09_模拟/JZ61_扑克牌的顺子.java
+++ app/app-learncode/src/main/java/com/open/learncode/算法/java/剑指offer/A09_模拟/JZ61_扑克牌的顺子.java
@@ -11,6 +11,7 @@ import java.util.Set;
  * <p>
  * 解题思路：
  * 集合Set+遍历，排序+遍历
+ * 特殊顺子：10 11 12 13 1(A)
  * <p>
  * 复杂度分析：
  * 方法一：
@@ -23,46 +24,40 @@ import java.util.Set;
 public class JZ61_扑克牌的顺子 {
 
     public static void main(String[] args) {
-//        int[] nums = {8, 0, 12, 11, 0};
-        int[] nums = {1, 2, 0, 0, 0};
+        int[] nums = {9, 11, 12, 13, 1};
+        int[] nums2 = {10, 11, 12, 13, 1};
+        int[] nums3 = {10, 11, 12, 9, 13};
         System.out.println("是否是顺子：" + method_1(nums));
-        System.out.println("是否是顺子：" + method_2(nums));
+        System.out.println("是否是顺子：" + method_1(nums2));
+        System.out.println("是否是顺子：" + method_1(nums3));
     }
 
     /**
-     * 集合Set+遍历
-     *
-     * ①若max-min>=5，肯定不是顺子
-     * ②若max-min<5，
-     * 1'若有重复元素（通过set实现遍历判重），肯定不是顺子
-     * 2'若无重复元素，要么似{8, 9, 12, 11, 10}；要么其中某些数字由0充当，似{8, 0, 12, 11, 0}
-     *
      * @param nums 待输入的数组
      * @return 返回是否是顺子，true为是顺子，false为不是顺子
      */
     public static boolean method_1(int[] nums) {
-
-        Set<Integer> repeat = new HashSet<>();
-
-        //借助max min变量，遍历得最大、小牌
-        //初始值：max为0（最小排面），min为14（最大排面）
-        int max = 0, min = 14;
-
+        // 这里选择数组来帮助判断，是因为数组的查找效率最高
+        int[] arr = new int[14];
+        int min = 14, max = 0;
+        boolean isSpecial = false;
         for (int num : nums) {
-            // 跳过大小王
-            if (num == 0)
-                continue;
-            max = Math.max(max, num); // 最大牌
-            min = Math.min(min, num); // 最小牌
-
-            // 若有重复，提前返回 false
-            if (repeat.contains(num))
+            // 遇到大小王，直接返回
+            if (num == -1) return false;
+            // 对 1(A) 特殊处理
+            if (num == 1) {
+                isSpecial = true;
+            } else {
+                min = Math.min(min, num);
+            }
+            max = Math.max(max, num);
+            // 有重复的
+            if (arr[num] != 0) {
                 return false;
-
-            // 若无重复，添加此牌至 Set
-            repeat.add(num);
+            }
+            arr[num] = num;
         }
-        return max - min < 5; // 最大牌 - 最小牌 < 5 则可构成顺子
+        return (!isSpecial && max - min == 4) || (isSpecial && max - min == 3);
     }
 
     /**
