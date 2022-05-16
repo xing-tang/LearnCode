ec1d01cfcf98ff2bacbb276598096e4d86b32ab4
mengjia.tang <mengjia.tang@appshahe.com>
2022-05-16 20:43:36 +0800

    mod: 合并两个排序数组修改.

--- app/app-learncode/src/main/java/com/open/learncode/算法/java/剑指offer/A10_其它算法/JZ05_2_合并两个排序数组.java
+++ app/app-learncode/src/main/java/com/open/learncode/算法/java/剑指offer/A10_其它算法/JZ05_2_合并两个排序数组.java
@@ -4,9 +4,10 @@ import com.open.learncode.算法.base.PrintUtils;
 
 /**
  * 题目：
- * 有两个排序的数组A1和A2，内存在A1的末尾有足够多的空余空间容纳A2。
- * 请实现一个函数，把A2中所有数字插入A1中，并且所有的数字都是排序的。
- * 例如：输入A1=>{1, 2, 3, 4}，A2=>{2, 4, 6}，输出A1=>{1, 2, 2, 3, 4, 4, 6}。
+ * 给你两个有序整数数组 nums1 和 nums2，请你将 nums2 合并到 nums1 中，使 nums1 成为一个有序数组。
+ * 初始化 nums1 和 nums2 的元素数量分别为 m 和 n 。你可以假设 nums1 的空间大小等于 m + n，这样它就有足够的空间保存来自 nums2 的元素。
+ * 示例 1：
+ * 输入：nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
  * <p>
  * 解题思路：
  * 从后往前替代+双指针
@@ -29,53 +30,46 @@ public class JZ05_2_合并两个排序数组 {
     /**
      * 从后往前替代+双指针
      * 时间复杂度 O(m+n)
-     *
-     * @param A1     输入的数组A1
-     * @param A1Size A1数组的真实长度
-     * @param A2     输入的数组A2
-     * @return 返回合并后的数组A1
      */
-    public static int[] method(int[] A1, int A1Size, int[] A2) {
-        if (A1 == null || A1.length <= 0) return A2;
-        if (A2 == null || A2.length <= 0) return A1;
-        if (A1.length < (A1Size + A2.length)) return null;
-        int a1Index = A1Size - 1;
-        int a2Index = A2.length - 1;
-        int len = a1Index + a2Index + 1;
-        while (a1Index >= 0 && a2Index >= 0) {
-            A1[len--] = A1[a1Index] > A2[a2Index] ? A1[a1Index--] : A2[a2Index--];
+    public static int[] method(int[] nums1, int m, int[] nums2, int n) {
+        if (nums1 == null || nums1.length <= 0) return nums2;
+        if (nums2 == null || nums2.length <= 0) return nums1;
+        int i = m - 1, j = n - 1, index = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            nums1[index--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--];
         }
-        while (a1Index >= 0) {
-            A1[len--] = A1[a1Index];
-            if (a1Index == 0) break;
-            a1Index--;
+        while (i >= 0) {
+            nums1[index--] = nums1[i];
+            if (i == 0) break;
+            i--;
         }
-        while (a2Index >= 0) {
-            A1[len--] = A2[a2Index];
-            if (a2Index == 0) break;
-            a2Index--;
+        while (j >= 0) {
+            nums1[index--] = nums2[j];
+            if (j == 0) break;
+            j--;
         }
-        return A1;
+        return nums1;
     }
 
     /**
+     * 从后往前替代+双指针
      * 时间复杂度 O(m+n)
      */
     public static int[] method2(int[] nums1, int m, int[] nums2, int n) {
-        int p1 = m - 1, p2 = n - 1;
-        int tail = m + n - 1;
-        int cur;
-        while (p1 >= 0 || p2 >= 0) {
-            if (p1 == -1) {
-                cur = nums2[p2--];
-            } else if (p2 == -1) {
-                cur = nums1[p1--];
-            } else if (nums1[p1] > nums2[p2]) {
-                cur = nums1[p1--];
+        if (nums1 == null || nums1.length <= 0) return nums2;
+        if (nums2 == null || nums2.length <= 0) return nums1;
+        int i = m - 1, j = n - 1, index = m + n - 1, cur;
+        while (i >= 0 || j >= 0) {
+            if (i == -1) {
+                cur = nums2[j--];
+            } else if (j == -1) {
+                cur = nums1[i--];
+            } else if (nums1[i] > nums2[j]) {
+                cur = nums1[i--];
             } else {
-                cur = nums2[p2--];
+                cur = nums2[j--];
             }
-            nums1[tail--] = cur;
+            nums1[index--] = cur;
         }
         return nums1;
     }
