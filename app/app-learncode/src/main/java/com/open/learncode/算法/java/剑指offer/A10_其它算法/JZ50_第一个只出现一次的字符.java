61714921daed955ce5511c4f84c11252dbf58526
mengjia.tang <mengjia.tang@appshahe.com>
2022-05-16 22:14:43 +0800

    mod: 第一个只出现一次的字符新解法-LinkedHashMap or HashMap.

--- app/app-learncode/src/main/java/com/open/learncode/算法/java/剑指offer/A10_其它算法/JZ50_第一个只出现一次的字符.java
+++ app/app-learncode/src/main/java/com/open/learncode/算法/java/剑指offer/A10_其它算法/JZ50_第一个只出现一次的字符.java
@@ -2,6 +2,7 @@ package com.open.learncode.算法.java.剑指offer.A10_其它算法;
 
 import com.open.learncode.算法.base.PrintUtils;
 
+import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -25,19 +26,28 @@ import java.util.Map;
  * 复杂度分析：
  * 时间复杂度：O(n)。
  * 空间复杂度：O(n)。
+ * 力扣：https://leetcode.cn/problems/di-yi-ge-zhi-chu-xian-yi-ci-de-zi-fu-lcof/
  */
 public class JZ50_第一个只出现一次的字符 {
 
     public static void main(String[] args) {
         // 测试用例
-        String str1 = "google";
-        String str2 = "Aa";
+        String str1 = "googlez";
+        String str2 = "AAAlaz";
         String str3 = "";
         PrintUtils.getInstance().print(solution(str1), "输出");
         PrintUtils.getInstance().print(solution(str2), "输出");
         PrintUtils.getInstance().print(solution(str3), "输出");
+        System.out.println();
+        PrintUtils.getInstance().print(solution2(str1), "输出");
+        PrintUtils.getInstance().print(solution2(str2), "输出");
+        PrintUtils.getInstance().print(solution2(str3), "输出");
     }
 
+    /**
+     * LinkedHashMap
+     * 直接返回不重复的第一个元素的下标
+     */
     private static int solution(String str) {
         if (str == null || str.length() <= 0) return -1;
 
@@ -55,5 +65,28 @@ public class JZ50_第一个只出现一次的字符 {
         }
 
         return -1;
+    }
+
+    /**
+     * HashMap
+     * 遍历，找到不重复且最小的下标
+     */
+    public static int solution2(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char key = s.charAt(i);
+            if (map.containsKey(key))
+                map.put(key, -1);
+            else
+                map.put(key, i);
+        }
+        int first = s.length();
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            int pos = entry.getValue();
+
+            if (pos != -1 && pos < first)
+                first = pos;
+        }
+        return first == s.length() ? -1 : first;
     }
 }
